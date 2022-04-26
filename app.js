f1865a6ba0a7666d83d6ec74944e7b3f68e2b070
camilo-micheletto <camilo.micheletto@passeidireto.com>
2022-04-26 17:46:26 -0300

    fix: fix port error

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const { App } = require("@slack/bolt");
 const { parsedDoc } = require("./services/gsServices");
 const express = require('express');
 const eapp = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-  port: process.env.PORT || 3000,
+  port
 });
 
 const getPatterns = async () => {
